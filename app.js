f7e149b90967773f3fd81b1dd364259fc6f6508c
Rayan <raeinking2@gmail.com>
2022-03-27 18:10:39 +0300

    all done

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const you = document.getElementById('youfight')
 const imgComputer = document.createElement('img')
 const imgYou = document.createElement('img')
 
+let computerspick = ''
+let youpick = ''
+
+let ElComputer = document.getElementById('computerScore')
+let Elyou = document.getElementById('youScore')
+
+let youScore = 0 ;
+let computerScore = 0 ;
+
 rock.addEventListener('click', (e) => {
     const imgSrc = e.target.attributes.src.nodeValue
     imgYou.setAttribute('src', imgSrc)
@@ -15,6 +24,14 @@ rock.addEventListener('click', (e) => {
     imgYou.setAttribute('srcset', ' ') 
     you.append(imgYou)   
     computerpick()
+    if (computerspick == './siccer.png') {
+        youScore += 1
+        Elyou.textContent = youScore
+    }
+    if (computerspick == './hand.png') {
+        computerScore += 1
+        ElComputer.textContent = computerScore
+    }
 })
 
 paper.addEventListener('click', (e) => {
@@ -24,6 +41,14 @@ paper.addEventListener('click', (e) => {
     imgYou.setAttribute('srcset', ' ') 
     you.append(imgYou)   
     computerpick()
+    if (computerspick == './rock.png') {
+        youScore += 1
+        Elyou.textContent = youScore
+    }
+    if (computerspick == './siccer.png') {
+        computerScore += 1
+        ElComputer.textContent = computerScore
+    }
 })
 
 siccer.addEventListener('click', (e) => {
@@ -33,6 +58,14 @@ siccer.addEventListener('click', (e) => {
     imgYou.setAttribute('srcset', ' ') 
     you.append(imgYou)   
     computerpick()
+    if (computerspick == './hand.png') {
+        youScore += 1
+        Elyou.textContent = youScore
+    }
+    if (computerspick == './rock.png') {
+        computerScore += 1
+        ElComputer.textContent = computerScore
+    }
 })
 
 const arrImg = ["./rock.png","./hand.png","./siccer.png"]
@@ -43,4 +76,6 @@ function computerpick() {
     imgComputer.setAttribute('alt', ' ')
     imgComputer.setAttribute('srcset', ' ')
     computer.append(imgComputer)
+    computerspick = '';
+    computerspick += arrImg[random]
 }
